7b81122efb9a9035212ada2ffd8843e3a817706c
Ashwini Sukale <ashwinisukale16@gmail.com>
2022-06-22 15:04:17 +0530

    RAU-78 Fixed the integration test case

--- spec/roles_on_routes/roles_on_routes_feature_spec.rb
+++ spec/roles_on_routes/roles_on_routes_feature_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 require 'roles_on_routes/roles_on_routes_controller'
 
 describe 'roles_on_routes integration', :type => :feature do
+  before do
+    # code ref - roles_on_routes/lib/roles_on_routes/configuration.rb
+    # In this file if @routeset_containing_roles is nil then it takes routes from rails application.
+    # Other test cases are modifying this variable when we execute whole test suite,
+    # hence we are making sure its not defined before running the test case.
+    RolesOnRoutes::Configuration.routeset_containing_roles = nil
+  end
   scenario 'visit inside' do
     visit inside_path
     expect(page).to have_text("This is animals/main/index")
